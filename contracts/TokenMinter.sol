9b503665f6117bf8226c7f482947f20f34164184
obichuk <cryptoman.aloha@gmail.com>
2022-04-08 14:09:11 +0300

    Testing TokenMinter in progress

--- contracts/TokenMinter.sol
+++ contracts/TokenMinter.sol
@@ -4,45 +4,96 @@ pragma solidity ^0.8.3;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 import "hardhat/console.sol";
 
-contract TokenMinter is ERC721Enumerable, Ownable {
+contract TokenMinter is ERC721Enumerable, Ownable, ReentrancyGuard {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
-    uint256 public mintRate = 0.3 ether;
     uint256 public MAX_SUPPLY = 300;
     bool public paused = false;
     bool public revealed = false;
     string baseURI;
 
-    constructor() ERC721("qqwe", "sxz") {}
+    SaleConfig public saleConfig;
 
-    function mint(uint256 _count) public payable {
-        require(totalSupply() < MAX_SUPPLY, "Can`t mint more");
-        require(msg.value <= mintRate, "Check you balance");
-        require(!paused, "Sale is paused");
+    struct AddressData {
+        uint128 balance;
+        uint128 numberMinted;
+    }
+
+    struct SaleConfig {
+        uint32 saleTime;
+        uint256 mintRate;
+    }
+
+    mapping(address => AddressData) private _addressData;
+
+    constructor() ERC721("qqwe", "sxz") {
+        saleConfig.saleTime = 1640444162;
+        saleConfig.mintRate = 0.3 ether;
+    }
+
+    function mint() public payable {
+        uint256 _saleStartTime = uint256(saleConfig.saleTime);
+        require(
+            _saleStartTime != 0 && block.timestamp >= _saleStartTime,
+            "sale has not started yet"
+        );
+
         uint256 tokenId = _tokenIdCounter.current();
+        address to = msg.sender;
 
+        require(totalSupply() < MAX_SUPPLY, "Can`t mint more");
+        require(msg.value <= saleConfig.mintRate, "Check you balance");
+        require(!paused, "Sale is paused");
+
+        AddressData memory addressData = _addressData[to];
+
+        _addressData[to] = AddressData(
+            addressData.balance + 1,
+            addressData.numberMinted + 1
+        );
         _tokenIdCounter.increment();
-        for (uint256 i = 0; i < _count; i++) {
-            _safeMint(msg.sender, tokenId);
-        }
+
+        _safeMint(to, tokenId);
     }
 
-    function helloWorld() public pure returns (string memory) {
-        return "HELLO WORLD2";
+    function myMintedNumber() public view returns (uint256) {
+        require(
+            msg.sender != address(0),
+            "ERC721A: number minted query for the zero address"
+        );
+        return uint256(_addressData[msg.sender].numberMinted);
     }
-    function getPauseState() public view returns(bool){
+
+    function numberMintedByAddress(address owner)
+        public
+        view
+        returns (uint256)
+    {
+        require(
+            owner != address(0),
+            "ERC721A: number minted query for the zero address"
+        );
+        return uint256(_addressData[owner].numberMinted);
+    }
+
+    function setStartTime(uint32 saleTime) external onlyOwner {
+        saleConfig.saleTime = saleTime;
+    }
+
+    function getPauseState() public view returns (bool) {
         return paused;
     }
 
-    function pause(bool _state) public onlyOwner {
+    function setPause(bool _state) public onlyOwner {
         paused = _state;
     }
 
     function setMintRate(uint256 _rate) public onlyOwner {
-        mintRate = _rate;
+        saleConfig.mintRate = _rate;
     }
 }
--- src/components/Minter/minter.tsx
+++ src/components/Minter/minter.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import { create as ipfsHttpClient } from 'ipfs-http-client'
-import { ethers } from 'ethers'
+import { Contract, ethers } from 'ethers'
 import Web3Modal from 'web3modal'
-import { Image } from 'antd';
+import { Button, Image, Radio } from 'antd';
 import {
     MINT_CONTRACT
 } from '../../../config.js'
@@ -12,30 +12,67 @@ import { useMoralis } from 'react-moralis';
 import { notification } from 'antd';
 export const Minter = () => {
     const { user, Moralis } = useMoralis();
-    const [mintRate, setMintRate] = useState(0);
-    const [mintedPieces, setMintedPieced] = useState(0);
-    const [maxSupply, setMaxSupply] = useState(0);
-    const [isBlocked, setBlocked] = useState(false);
+
+    const [mintRate, setMintRate] = useState<number>(0);
+    const [mintedPieces, setMintedPieced] = useState<number>(0);
+    const [maxSupply, setMaxSupply] = useState<number>(0);
+    const [isBlocked, setBlocked] = useState<boolean>(false);
+    const [mintedCountByAddress, setMintedCountByAddress] = useState<number>(0);
+    const [saleStartedAt, setSaleStartedAt] = useState<number>();
+
 
     React.useEffect(
         () => {
             async function readContractData() {
-                const signer = await Web3Service.getMySigner()
-                let contract = new ethers.Contract(MINT_CONTRACT, TokenMinter.abi, signer);
-                let rate = await contract.mintRate();
-                setMintRate(+Moralis.Units.FromWei(rate));
+                const web3Provider = await Moralis.enableWeb3();
+                const contract = new ethers.Contract(MINT_CONTRACT, TokenMinter.abi, web3Provider);
+
+                let config = await contract.saleConfig();
+                debugger;
+
+                setSaleStartedAt(config.saleTime);
+                setMintRate(+Moralis.Units.FromWei(config.mintRate));
                 setMintedPieced((await contract.totalSupply()).toString())
                 setMaxSupply((await contract.MAX_SUPPLY()).toString())
                 setBlocked(((await contract.getPauseState()).toString()))
+
+                setMintedCountByAddress((await contract.numberMintedByAddress()).toString())
             }
 
             readContractData();
-
         }, []
     );
 
-    async function mint() {
+    async function doTransaction(doTx: any, onSuccess: any, onFail: any) {
+        try {
+            let transaction = await doTx();
 
+            let tx = await transaction.wait();
+            if (tx)
+                onSuccess();
+        } catch (e: any) {
+            onFail(e);
+        }
+    };
+
+    async function doPause(state: boolean) {
+        const signer = await Web3Service.getMySigner()
+        let contract = new ethers.Contract(MINT_CONTRACT, TokenMinter.abi, signer);
+        await doTransaction(
+            async () => await contract.pause(state),
+            () => {
+                notification.success({
+                    message: `Pause tx completed.`,
+                });
+            }, (e: any) => {
+                notification.error({
+                    message: `${e.data.message}`,
+                });
+            });
+    };
+
+    async function doMint() {
+        debugger;
         const signer = await Web3Service.getMySigner()
         let contract = new ethers.Contract(MINT_CONTRACT, TokenMinter.abi, signer);
         contract.filters.Transfer(user?.get('ethAddress'));
@@ -46,56 +83,48 @@ export const Minter = () => {
             setMintedPieced((await contract.totalSupply()).toString())
         });
 
-        try {
-            let transaction = await contract.mint(1)
-
-            let tx = await transaction.wait();
-            if (tx) {
+        await doTransaction(
+            async () => await contract.mint(1),
+            () => {
                 notification.success({
                     message: `Transaction executed`,
                 });
-            }
-        } catch (e: any) {
-            notification.error({
-                message: `Transaction failer ${e.data}`,
+            }, (e: any) => {
+                notification.error({
+                    message: `Transaction failer ${e.data.message}`,
+                });
             });
-            console.log(e.data);
-        }
-    }
-
-
-
-    const Web3Service = {
-        getMySigner: async function () {
-            const web3Modal = new Web3Modal()
-            const connection = await web3Modal.connect()
-            const provider = new ethers.providers.Web3Provider(connection)
-            return provider.getSigner()
-        }
     };
 
     return (
         <div>
-
-
-
             <div className="grid p-4 grid-cols-1 hover:grid-cols-6">
                 <div className='text-center'>
                     <div className='mt-4'>
                         <div>
-                            <Image preview={false} height={"250px"} width={"250px"} src='preview.gif' className='block'></Image>
+                            <Image preview={false} height={"50px"} width={"50px"} src='preview.gif' className='block'></Image>
                         </div>
                         <div className='mt-4'   >
                             <Image preview={false} height={"50px"} width={"50px"} src='question-mark.png' className='block'></Image>
                         </div>
                         <p className='p-1'>Mint you random NFT. One from the 300 token could be you.</p>
                         <h3>Price per mint: {mintRate}</h3>
+                        <h3>Started At: {saleStartedAt}</h3>
                         <h3>Minting live: {isBlocked ? "live" : "paused"}</h3>
                         <h3>Supply: {mintedPieces}/{maxSupply}</h3>
-                        <button className="font-bold p-4 mt-4 bg-pink-500 text-white rounded  shadow-lg" onClick={mint}> Mint you unique NFT</button>
+                        <h3>Minted by you account :{mintedCountByAddress}</h3>
+                        <Button className="font-bold p-4 mt-4 bg-pink-500 text-white rounded  shadow-lg" onClick={doMint}>Mint you unique NFT</Button>
 
                     </div>
 
+                    <div className=''>
+                        {
+                            <p>Right now contract sale is : {isBlocked ? 'on' : 'off'}</p>
+                        }
+                        <Button className="m-4 font-bold p-4 mt-4 bg-pink-500 text-white rounded  shadow-lg" disabled={!isBlocked} onClick={() => doPause(true)}> Pause off</Button>
+                        <Button className="m-4 font-bold p-4 mt-4 bg-pink-500 text-white rounded  shadow-lg" disabled={isBlocked} onClick={() => doPause(false)}> Pause on</Button>
+
+                    </div>
 
                 </div>
             </div>
@@ -104,3 +133,12 @@ export const Minter = () => {
 
     )
 }
+
+const Web3Service = {
+    getMySigner: async function () {
+        const web3Modal = new Web3Modal()
+        const connection = await web3Modal.connect()
+        const provider = new ethers.providers.Web3Provider(connection)
+        return provider.getSigner();
+    }
+};
